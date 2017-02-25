6a33e3407c484ccec7ce8eb91054de29b12da175
Anubhuti Agrawal <aagrawal@internal.tavisca.com>
2017-02-25 14:49:19 +0530

    Initial commit

---
+++ Palindrom2/Palindrom2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom2
+{
+    class palindromNum
+    {
+        static void Main(string[] args)
+        {
+            int num, rem, sum = 0, tempval;
+            Console.Write("Enter a number ");
+            num = Convert.ToInt32(Console.ReadLine());
+            tempval = num;
+            while (num > 0)
+            {
+                rem = num % 10;  
+                num = num / 10;
+                sum = sum * 10 + rem; 
+                           
+            }
+           
+            if (tempval==sum)
+            {
+                Console.WriteLine(" Number is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Number is not a palindrome");
+            }
+           
+
+
+        }
+    }
+}
---
+++ PrimeNo/PrimeNo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNo
+{
+    class Prime
+    {
+        static void Main(string[] args)
+        { 
+         Console.Write("Enter a Number");
+            int num, count = 0;
+        num = Convert.ToInt32(Console.ReadLine());
+          
+       
+            for (int i = 1; i <= num/2; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+}
+            if (count==2)
+            { 
+                Console.WriteLine("Entered Number is a Prime Number");
+            }
+            else
+            {
+                Console.WriteLine("Not a Prime Number");
+            }
+          
+        }
+    }
+}
+           
